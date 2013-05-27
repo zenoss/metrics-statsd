62c874b61413e39f23528a42f4aedb8134d5df34
Mike Keesey <michael.keesey@readytalk.com>
2013-05-27 16:59:19 -0600

    Split out a lot of the serialization into StatsdSerializer and made it long based (Statsd wants you to send it longs, not doubles or floats).

--- src/main/java/com/bealetech/metrics/reporting/StatsdReporter.java
+++ src/main/java/com/bealetech/metrics/reporting/StatsdReporter.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2012-2013 Sean Laurent
+ * Copyright (C) 2013 Michael Keesey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
--- src/main/java/com/bealetech/metrics/reporting/StatsdSerializer.java
+++ src/main/java/com/bealetech/metrics/reporting/StatsdSerializer.java
@@ -1,9 +1,22 @@
+/**
+ * Copyright (C) 2012-2013 Sean Laurent
+ * Copyright (C) 2013 Michael Keesey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.bealetech.metrics.reporting;
 
 import com.yammer.metrics.core.MetricName;
-import com.yammer.metrics.core.Sampling;
-import com.yammer.metrics.core.Summarizable;
-import com.yammer.metrics.stats.Snapshot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,66 +40,23 @@ public class StatsdSerializer {
 		this.prependNewline = false;
 	}
 
-	protected void sendSummarizable(String sanitizedName, Summarizable metric) {
-		sendFloat(sanitizedName + ".min", StatType.TIMER, metric.min());
-		sendFloat(sanitizedName + ".max", StatType.TIMER, metric.max());
-		sendFloat(sanitizedName + ".mean", StatType.TIMER, metric.mean());
-		sendFloat(sanitizedName + ".stddev", StatType.TIMER, metric.stdDev());
+	public void writeGauge(String metricName, long value) {
+		//TODO bounds checking
+		writeData(metricName, Long.toString(value, 10), StatType.GAUGE);
 	}
 
-	protected void sendSampling(String sanitizedName, Sampling metric) {
-		final Snapshot snapshot = metric.getSnapshot();
-		sendFloat(sanitizedName + ".median", StatType.TIMER, snapshot.getMedian());
-		sendFloat(sanitizedName + ".75percentile", StatType.TIMER, snapshot.get75thPercentile());
-		sendFloat(sanitizedName + ".95percentile", StatType.TIMER, snapshot.get95thPercentile());
-		sendFloat(sanitizedName + ".98percentile", StatType.TIMER, snapshot.get98thPercentile());
-		sendFloat(sanitizedName + ".99percentile", StatType.TIMER, snapshot.get99thPercentile());
-		sendFloat(sanitizedName + ".999percentile", StatType.TIMER, snapshot.get999thPercentile());
+	//TODO counters
+
+	public void writeTimer(String metricName, long timeInMS) {
+		//TODO bounds checking
+		writeData(metricName, Long.toString(timeInMS, 10), StatType.TIMER);
 	}
 
-	protected void sendInt(String name, StatType statType, long value) {
-		sendData(name, String.format(locale, "%d", value), statType);
-	}
-
-	protected void sendFloat(String name, StatType statType, double value) {
-		sendData(name, String.format(locale, "%2.2f", value), statType);
-	}
-
-	protected void sendObj(String name, StatType statType, Object value) {
-		sendData(name, String.format(locale, "%s", value), statType);
-	}
-
-	protected String sanitizeName(MetricName name) {
-		final StringBuilder sb = new StringBuilder()
-				.append(name.getGroup())
-				.append('.')
-				.append(name.getType())
-				.append('.');
-		if (name.hasScope()) {
-			sb.append(name.getScope())
-					.append('.');
-		}
-		return sb.append(name.getName()).toString();
-	}
-
-	protected String sanitizeString(String s) {
+	private String sanitizeString(String s) {
 		return s.replace(' ', '-');
 	}
 
-	protected void sendData(String name, String value, StatType statType) {
-		String statTypeStr = "";
-		switch (statType) {
-			case COUNTER:
-				statTypeStr = "c";
-				break;
-			case GAUGE:
-				statTypeStr = "g";
-				break;
-			case TIMER:
-				statTypeStr = "ms";
-				break;
-		}
-
+	private void writeData(String name, String value, StatType statType) {
 		try {
 			if (prependNewline) {
 				writer.write("\n");
@@ -98,7 +68,7 @@ public class StatsdSerializer {
 			writer.write(":");
 			writer.write(value);
 			writer.write("|");
-			writer.write(statTypeStr);
+			writer.write(statType.statsdType());
 			prependNewline = true;
 			writer.flush();
 		} catch (IOException e) {
