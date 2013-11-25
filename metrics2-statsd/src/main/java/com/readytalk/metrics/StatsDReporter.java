054a85285892afc96b80ad75c7df00a3554b88bb
Mike Keesey <michael.keesey@readytalk.com>
2013-11-25 16:09:12 -0700

    Fixing copyright notices

--- metrics2-statsd/src/main/java/com/readytalk/metrics/StatsDReporter.java
+++ metrics2-statsd/src/main/java/com/readytalk/metrics/StatsDReporter.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2012-2013 Sean Laurent
- * Copyright (C) 2013 Michael Keesey
+ * Copyright (C) 2013 metrics-statsd contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
